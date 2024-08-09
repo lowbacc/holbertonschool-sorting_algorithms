9fc214e5328c8b4f07267fc619c29a868425324c
CoeurdArcade <coeurdarcade@gmail.com>
2024-08-09 18:03:43 +0200

    script function that sorts an array of integers in ascending order using the shell sort algorithm, using the knuth sequence

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,38 +1,52 @@
 #include "sort.h"
 
 /**
- *shell_sort - sorts an array of integers in ascending order
- *using the Shell sort algorithm, using the Knuth sequence
- *@array: the array
- *@size: the size
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
  *
- *Return: Nothing
+ * Rturn: NULL.
+ */
+
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * shell_sort - Sort an array of integers in ascending
+ *              order using the shell sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Uses the Knuth interval sequence.
+ *
+ * Return: Void.
  */
 
 void shell_sort(int *array, size_t size)
 {
-	size_t h = 1, i, n;
-	int number;
+	size_t gap, a, b;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (h < size / 3)
-	{
-		h = h * 3 + 1;
-	}
+	for (gap = 1; gap < (size / 3);)
+		gap = gap * 3 + 1;
 
-	for (; h >= 1; h /= 3)
+	for (; gap >= 1; gap /= 3)
 	{
-		for (i = h; i < size; i++)
+		for (a = gap; a < size; a++)
 		{
-			n = i;
-			while (n >= h && array[n] < array[n - h])
+			b = a;
+			while (b >= gap && array[b - gap] > array[b])
 			{
-				number = array[n];
-				array[n] = array[n - h];
-				array[n - h] = number;
-				n -= h;
+				swap_ints(array + b, array + (b - gap));
+				b -= gap;
 			}
 		}
 		print_array(array, size);
