37b11434a4abc9b56aeb8633ca06963e8bf763b8
CoeurdArcade <coeurdarcade@gmail.com>
2024-08-09 18:09:41 +0200

    script function that sorts an array of integers in ascending order using the shell sort algorithm, using the knuth sequence

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,53 +1,41 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
- *
- * Rturn: NULL.
+ * shell_sort - sorts an array of integers in ascending order using a shell
+ * sort algorithm, with gap sizes determined by a decreasing Knuth seqeuence
+ * @array: array of integers to be sorted
+ * @size: amount of elements in array
  */
-
-void swap_ints(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
-/**
- * shell_sort - Sort an array of integers in ascending
- *              order using the shell sort algorithm.
- * @array: An array of integers.
- * @size: The size of the array.
- *
- * Description: Uses the Knuth interval sequence.
- *
- * Return: Void.
- */
-
 void shell_sort(int *array, size_t size)
 {
-	size_t gap, a, b;
+	int i, j, gap, n, Knuth_max, temp;
 
-	if (array == NULL || size < 2)
+	if (!array || size < 2)
 		return;
 
-	for (gap = 1; gap < (size / 3);)
-		gap = gap * 3 + 1;
-
-	for (; gap >= 1; gap /= 3)
+	n = (int)size;
+	for (gap = 1; gap < n; gap = (gap * 3) + 1)
 	{
-		for (a = gap; a < size; a++)
+		Knuth_max = gap;
+	}
+	/* Start with the largest Knuth seq value less than n as gap, */
+	/* and work down sequence to a gap of 1 */
+	for (gap = Knuth_max; gap > 0; gap = (gap - 1) / 3)
+	{
+		/* Do a gapped insertion sort for this gap size. */
+		for (i = gap; i < n; i++)
 		{
-			b = a;
-			while (b >= gap && array[b - gap] > array[b])
+			/* add array[i] to gap sorted elements; */
+			/* save array[i] in temp in preparation to overwrite */
+			temp = array[i];
+			/* shift earlier gap-sorted elements up until the */
+			/* correct location for array[i] is found */
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
 			{
-				swap_ints(array + b, array + (b - gap));
-				b -= gap;
+				array[j] = array[j - gap];
 			}
+			/* temp original array[i]) to its correct location */
+			array[j] = temp;
 		}
 		print_array(array, size);
 	}
